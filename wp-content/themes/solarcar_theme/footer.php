0661c9510a8f8d17d074d773bfc0a78ab02f7d98
Zouron <jasrego@gmail.com>
2016-12-06 23:04:32 +1100

    Add abolute URL to social icons. Icons now displaying white

--- wp-content/themes/solarcar_theme/footer.php
+++ wp-content/themes/solarcar_theme/footer.php
@@ -1,39 +1,28 @@
-		<div id="footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="social-icon-box center-items">
-							<div class="">
-								<img src="<?php echo get_template_directory_uri() ; ?>/images/WSU_white_left.png" alt="WSU Shield">
-							</div>
-							
-							<div id="icon-row">	
-								<a href="https://www.facebook.com/westsydusolarcarproject/"><img src="<?php echo get_template_directory_uri() ; ?>/images/facebook.png" alt="facebook logo" width="35" height="35"></a>
-							
-								<a href="https://www.linkedin.com/company/6425091"><img src="<?php echo get_template_directory_uri() ; ?>/images/linkedin.png" alt="linkedin logo" width="35" height="35"></a>
-							
-								<a href=""><img src="<?php echo get_template_directory_uri() ; ?>/images/instagram.png" alt="instagram logo" width="35" height="35"></a>
-							
-								<a href=""><img src="<?php echo get_template_directory_uri() ; ?>/images/twitter.png" alt="twitter logo" width="" height=""></a>
-							
-							</div>
-						</div>
-					</div>				
-					<div class="col-md-6">						
-						<form class="form-horizontal">
-							<div class="form-group">
-								
-									<input type="text" class="form-control center-items" style="width: 250px" name="UserEmail" placeholder="Your Email Address">
-								
-								
-									<input class="btn btn-default" id="sign-up-btn" type="submit" name="submit" value="Subcribe">
-								
-							</div>
-						</form>
-					</div>
-				</div>
-			</div><!--END container-->
-		</div><!--END footer-->
+		<footer>
+      	<section>
+      		<img src="./img/WSU_white_left.png" alt="Western Sydney University" />
+
+      		<div id="social">
+      			<a class="facebook_footer" href="https://www.facebook.com/westsydusolarcarproject" title="Facebook"><img src="<?php echo get_template_directory_uri() ?>/images/facebook.png" alt="Facebook" /> </a>
+      			<a class="linkedin_footer" href="https://www.linkedin.com/company/uws-solar-car-project" title="LinkedIn"><img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" /> </a>
+      			<a class="instagram_footer"href="http://instagram.com/westsydusolarcar" title="Instagram"><img src="<?php echo get_template_directory_uri() ?>/images/instagram.png" alt="Instagram" /> </a>
+      			<a class="twitter_footer" href="https://twitter.com/WestSydSolarCar" title="Twitter"><img src="<?php echo get_template_directory_uri() ?>/images/twitter.png" alt="Twitter" /> </a>
+      			<a class="youtube_footer"href="https://www.youtube.com/channel/UCYsLuiDrqBru3A6V5OLZRBw" title="Youtube"><img src="<?php echo get_template_directory_uri() ?>/images/youtube.png" alt="Youtube" /> </a>
+      		</div>
+      		<div id="sitemap">
+      			<ul>
+      				<li><a href="#top">HOME</a></li> |
+      				<li><a href="#team">TEAM</a></li> |
+                    <li><a href="#alumni">ALUMNI</a></li> |
+      				<li><a href="#car">CAR</a></li> |
+      				<li><a href="#partners">PARTNERS</a></li> |
+       				<li><a href="#news">NEWS</a></li>|
+      				<li><a href="#contact">CONTACT US</a></li> 
+      			</ul>
+      			&#9400; Western Sydney University Solar Car Project 2016 | All Rights Reserved
+      		</div>
+      	</section>
+      </footer>
 		<script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
