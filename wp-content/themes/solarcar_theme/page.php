aa437b17a2977261077df3e62e843384c44833c9
Jason Rego <jasonfelixrego@gmail.com>
2016-12-13 22:52:06 +1100

    Added counter for columns

---
+++ wp-content/themes/solarcar_theme/page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * @package WordPress
+ * @subpackage Solarcar
+ * @since 2017
+ */
+
+get_header(); ?>
+
+
+
+<?php
+	if ( is_front_page() && solarcar_theme_has_featured_posts() ) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+	//Starting the count as zero
+	$count=(int)0;
+?>
+<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			echo do_shortcode("[post_grid id='32']");
+
+ 		endwhile;?>
+ <?php else:?>
+		<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+
+
+<?php
+
+get_footer();
