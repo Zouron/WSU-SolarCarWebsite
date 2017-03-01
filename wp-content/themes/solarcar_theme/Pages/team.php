cbde8b8d122f36ddd3c935a75c27986a8a93e629
Jason Rego <jasonfelixrego@gmail.com>
2017-03-01 22:45:31 +1100

    Add team file, change file - Samuel

---
+++ wp-content/themes/solarcar_theme/Pages/team.php
@@ -0,0 +1,135 @@
+<section>
+      	<a name="team"></a>
+        <img class="wide_img" src="./img/team.jpg">
+        <h1>Meet the Team</h1>
+        <hr/>
+        <p>
+          Integrated within the university, we bring students out from a theoretical world into a more practical environment to apply their knowledge and expand their horizons. We are made up of undergraduate students from a variety of disciplines ranging from engineering, computing, business, media &amp; communications.
+        </p>
+        <div class="team">
+        	<div class="person">
+        		<div class="info">
+        			<div class="p_name">Samuel Larrondo</div>
+        			<div class="p_title">Team Manager</div>
+        			<div class="p_social">
+        				<a class="p_linkedin" href="https://www.linkedin.com/in/sam-larrondo- 014794125/">
+                  		  <img src=".<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+                        </a>
+					</div>
+        		</div>
+            <div class="portrait">
+              <img src=".<?php echo get_template_directory_uri() ?>/images/team/samuel.png" alt="Samuel Larrondo" />
+            </div>
+        	</div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Saamiul Bashar</div>
+              <div class="p_title">Engineering Lead</div>
+              <div class="p_social">
+                  <a class="p_linkedin" href="https://www.linkedin.com/in/saamiulbashar/">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+                  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/ann.jpg" alt="Ann Xiao" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Michael Jay</div>
+              <div class="p_title">Electrical Lead</div>
+              <div class="p_degree">Electrical Engineering</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/michael.jpg" alt="Michael Jay" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Cameron Weston</div>
+              <div class="p_title">Mechanical Lead</div>
+              <div class="p_degree">Mechanical Engineering</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/cam.jpg" alt="Cameron Weston" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Kieran Luken</div>
+              <div class="p_title">IT &amp; Telemetry Lead</div>
+              <div class="p_degree">Information &amp; Communications Technology</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/kieran.jpg" alt="Kieran Luken" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Rohan Munjal</div>
+              <div class="p_title">Mechanical</div>
+              <div class="p_degree">Mechanical Engineering</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/rohan.jpg" alt="Rohan Munjal" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Rez Pulido</div>
+              <div class="p_title">Mechanical</div>
+              <div class="p_degree">Mechanical Engineering</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/rez.jpg" alt="Rez Pulido" />
+            </div>
+          </div>
+          <div class="person">
+            <div class="info">
+              <div class="p_name">Daniel Eddington</div>
+              <div class="p_title">Electrical</div>
+              <div class="p_degree">Electrical Engineering</div>
+              <div class="p_bio"></div>
+              <div class="p_social">
+                  <a href="http://linkedin">
+                  	<img src="<?php echo get_template_directory_uri() ?>/images/linkedin.png" alt="LinkedIn" />
+				  </a>
+              </div>
+            </div>
+            <div class="portrait">
+              <img src="./img/team/d1.jpg" alt="Daniel Eddington" />
+            </div>
+          </div>
+        </div>
+      </section>
