91042e5cc3bfbdbc01bfef0ad63f1d61db890e59
Jason Rego <jasonfelixrego@gmail.com>
2017-03-03 08:17:14 +1100

    Add file or sponsors

---
+++ wp-content/themes/solarcar_theme/page-82.php
@@ -0,0 +1,67 @@
+<?php get_header(); ?>
+<section class="container">
+      	<a name="partners"></a>
+      	<div class="media-headings gothambold">Our Sponsors</div>
+      	<!--<h2>Join us for 2017.</h2>-->
+      	<p class="header-text">
+      		Following the success of our first foray, Western Sydney University is now investing in the project and invites private, corporate and government partners to do the same. Financial and in-kind support is welcome.
+      	</p>
+      	<p class="header-text">
+      		A range of sponsorship benefits are available. Interested individuals and organisations are invited to share in the success of this opportunity which only comes around every two years. With your help, we can get the Western Sydney University Solar Car on the road again for the 2017 World Solar Challenge.
+      	</p>
+        <p class="header-text">
+          To find out more, contact us at <b><a href="mailto:sponsorship@solarcar.scem.westernsydney.edu.au?Subject=Sponsorship%20Enquiry">sponsorship@solarcar.scem.westernsydney.edu.au</a></b>
+        </p>
+      	<p class="center">
+	      	<a class="donate" href="https://give.westernsydney.edu.au/solarcar">
+	      		DONATE
+	      	</a>
+      	</p>
+      	<div class="sponsor_class">
+      		<h1 id="platinum">Platinum</h1>
+      		<div class="logos">
+      			<a href = "http://www.scem.westernsydney.edu.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/scem.png" alt="WSU SCEM" /></a>
+      		</div>
+      	</div>
+      	<div class="sponsor_class">
+      		<h1 id="gold">Gold</h1>
+      		<div class="logos">
+      			<a href = "http://www.bridgestone.com.au"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/bridgestone.png" alt="Bridgestone" /></a>
+      			<a href = "http://www.ansys.com"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/ansys.jpg" alt="ANSYS" /></a>
+      			<a href = "http://www.sunpower.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/sunpower.png" alt="Sunpower" /></a>
+      		</div>
+      	</div>
+      	<div class="sponsor_class">
+      		<h1 id="silver">Silver</h1>
+      		<div class="logos">
+      			<a href = "http://www.westernsydney.edu.au/campuslife/campus_life"><img height="200px" src="<?php echo get_template_directory_uri() ?>/images/sponsors/wsucampuslife.png" alt="Campus Life" /></a>
+      			<a href = "http://www.nepinc.com/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/nep.png" alt="NEP" /></a>
+      			<a href = "http://www3.lenovo.com/au/en"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/lenovo.png" alt="Lenovo" /></a>
+      			<a href = "https://www.mystudentvillage.com/au/westernsydneyuv/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/clv.png" alt="Western Sydney University Village" /></a>
+      			<a href = "http://www.pivotel.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/pivotel.png" alt="Pivotel" /></a>
+      			<a href = "http://www.weatherzone.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/weatherzone.png" alt="Weatherzone" /></a>
+      			<a href = "http://www.lsm.net.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/lsm.png" alt="LSM Advanced Composites" /></a>
+      		</div>
+      	</div>
+      	<div class="sponsor_class">
+      		<h1 id="bronze">Bronze</h1>
+      		<div class="logos">
+      			<a href = "http://www.industry.nsw.gov.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/nswtradeinvestment.png" alt="NSW Department Of Industry" /></a>
+      			<a href = "https://www.penrithcity.nsw.gov.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/penrithcitycouncil.png" alt="Penrith City Council" /></a>
+      		</div>
+      	</div>
+      	<div class="sponsor_class">
+      		<h1 id="friends">Friends</h1>
+      		<div class="logos">
+      			<a href = "http://www.adobe.com/au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/adobe.png" alt="Adobe" /></a>
+      			<a href = "http://www.altium.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/altium.png" alt="Altium" /></a>
+      			<a href = "http://www.arrowprint.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/arrow.png" alt="Arrow Print" /></a>
+      			<a href = "https://www.dropbox.com/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/dropbox.png" alt="Dropbox" /></a>
+      			<a href = "http://www.gochermann.com/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/gochermann.png" alt="Gochermann Solar Technology" /></a>
+      			<a href = "http://www.leapaust.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/leap.png" alt="Leap Australia" /></a>
+      			<a href = "http://www.macarthurdrafting.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/macarthur.png" alt="Macarthur Design & Drafting" /></a>
+      			<a href = "http://www.sydneydragway.com.au/"><img src="<?php echo get_template_directory_uri() ?>/images/sponsors/sydney_dragway.png" alt="Sydney Dragway" /></a>
+      		</div>
+      	</div>
+      </section>
+<?php get_footer(); ?>
