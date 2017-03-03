cab99e2493b540959828392d53cc2f49f0c7c90d
Jason Rego <jasonfelixrego@gmail.com>
2017-03-03 14:58:33 +1100

    Initial create of car page

---
+++ wp-content/themes/solarcar_theme/page-80.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+<section>
+      <a name="car"></a>
+      	<h1>Meet Our Cars</h1>
+      	<h2>Driving into a sustainable future.</h2>
+      	<hr /> <br/>
+      <!--need to fix the position of the solace image transitioning with scrolling-->
+      	<figure class="cd-image-container">
+      		<img src="./img/unlimited.jpg" alt="Unlimited. (2015)">
+      		<span class="cd-image-label" data-type="original">UNLIMITED. (2015)</span>
+
+      		<div class="cd-resize-img"> <!-- the resizable image on top -->
+      			<img src="./img/solace.jpg" alt="Solace (2013)">
+      			<span class="cd-image-label" data-type="modified">Solace (2013)</span>
+      		</div>
+      		<span class="cd-handle"></span>
+      	</figure>
+      	<p>
+      	  Our racing cars showcase cutting-edge sustainable engineering and demonstrate the incredible potential of electric vehicle technologies. By designing a car to run on solar power alone, we are driving the changes in vehicle efficiency and new technologies for a low-carbon future.
+      	</p>
+      	<p>
+      	  Constructed from a carbon-nomex composite, the monocoque chassis of our solar car seats custom designed suspension components. We make use of these industry leading materials and techniques to create a race vehicle that is not only light, but strong as well.
+      	</p>
+      	<div id="facts_table">
+      		<table id="facts">
+      			<tr>
+      				<td class="table_left">300KG</td>
+      				<td class="table_centre">WEIGHT</td>
+      				<td class="table_right">170KG</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">CENTRAL MONOCOQUE</td>
+      				<td class="table_centre">DESIGN</td>
+      				<td class="table_right">CATAMARAN</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">1.8M (W) x 4.5M (L) x 1.1M (H)</td>
+      				<td class="table_centre">SIZE</td>
+      				<td class="table_right">1.8M (W) x 4.5M (L) x 0.9M (H)</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">110KM/H</td>
+      				<td class="table_centre">SPEED</td>
+      				<td class="table_right">129KM/H</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">CARBON FIBRE &amp; NOMEX HONEYCOMB</td>
+      				<td class="table_centre">STRUCTURE</td>
+      				<td class="table_right">CARBON FIBRE &amp; NOMEX HONEYCOMB</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">PANASONIC LITHIUM ION</td>
+      				<td class="table_centre">BATTERY</td>
+      				<td class="table_right">PANASONIC LITHIUM ION</td>
+      			</tr>
+      			<tr>
+      				<td class="table_left">MARAND IN-HUB DC</td>
+      				<td class="table_centre">MOTORS</td>
+      				<td class="table_right">MARAND IN-HUB DC</td>
+      			</tr>
+      		</table>
+      	</div>
+      </section>
+<?php get_footer(); ?>
