f9e840943e53be6bfe0ab9675aebcbe51233507b
Jason Rego <jasonfelixrego@gmail.com>
2017-03-10 16:35:23 +1100

    Move form to the footer

--- wp-content/themes/solarcar_theme/footer.php
+++ wp-content/themes/solarcar_theme/footer.php
@@ -1,27 +1,27 @@
 		<footer>
-		<div style="background-color: #ffffff; display: none;">
-					<!-- Begin MailChimp Signup Form -->
-					
-					<form action="//uws.us11.list-manage.com/subscribe/post?u=b6a07cf7fb5a609de48c04c60&amp;id=4c17b21603" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-						<div id="mc_embed_signup_scroll">
-						<p>To keep up to date, subscribe to our newsletter:</p>
-					<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-					<input type="email" placeholder="Enter Email Address *" name="EMAIL" class="subEmail" id="mce-EMAIL"><br/>
-						<input type="text" placeholder="Enter First Name" name="FNAME" class="subFName" id="mce-FNAME"><br/>
-						<input type="text" placeholder="Enter Last Name" name="LNAME" class="subLName" id="mce-LNAME"><br/><br/>
-						<div id="mce-responses" class="clear">
-							<div class="response" id="mce-error-response" style="display:none"></div>
-							<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b6a07cf7fb5a609de48c04c60_4c17b21603" tabindex="-1" value=""></div>
-						<input id="submit" type="submit" name="submit" class = "submit" value="SUBSCRIBE" />
-						</div>
-					</form>
-					<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-					<!--End mc_embed_signup-->				
-			</div>
       	<section>
       		<img src="<?php echo get_template_directory_uri() ?>/images/WSU_white_left.png" alt="Western Sydney University" />
+                  <div style="background-color:">
+                              <!-- Begin MailChimp Signup Form -->
+                              
+                              <form action="//uws.us11.list-manage.com/subscribe/post?u=b6a07cf7fb5a609de48c04c60&amp;id=4c17b21603" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div id="mc_embed_signup_scroll">
+                                    <p>To keep up to date, subscribe to our newsletter:</p>
+                              <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                              <input type="email" placeholder="Enter Email Address *" name="EMAIL" class="subEmail" id="mce-EMAIL"><br/>
+                                    <input type="text" placeholder="Enter First Name" name="FNAME" class="subFName" id="mce-FNAME"><br/>
+                                    <input type="text" placeholder="Enter Last Name" name="LNAME" class="subLName" id="mce-LNAME"><br/><br/>
+                                    <div id="mce-responses" class="clear">
+                                          <div class="response" id="mce-error-response" style="display:none"></div>
+                                          <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b6a07cf7fb5a609de48c04c60_4c17b21603" tabindex="-1" value=""></div>
+                                    <input id="submit" type="submit" name="submit" class = "submit" value="SUBSCRIBE" />
+                                    </div>
+                              </form>
+                              <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                              <!--End mc_embed_signup-->                      
+                  </div>
 
       		<div id="social">
       			<a class="facebook_footer" href="https://www.facebook.com/westsydusolarcarproject" title="Facebook"><img src="<?php echo get_template_directory_uri() ?>/images/facebook.png" alt="Facebook" /> </a>
